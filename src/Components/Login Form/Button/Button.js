0ce458e5ed3e548949b13c117a654f178ca8e6ba
murli777 <bhardwajmd1@outlook.com>
2024-11-08 19:27:27 +0530

    pass props for button type and text to make more reusable

--- src/Components/Login Form/Button/Button.js
+++ src/Components/Login Form/Button/Button.js
@@ -1,13 +1,14 @@
-import { Component } from 'react'
+import { Component } from "react";
 
 export class Button extends Component {
   render() {
+    const { type, text } = this.props;
     return (
       <>
-        <button type="submit">Submit</button>
+        <button type={type}>{text}</button>
       </>
-    )
+    );
   }
 }
 
-export default Button
+export default Button;
