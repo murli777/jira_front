00061fb15eea2566584c0fcab0bfc035728485ef
murli777 <bhardwajmd1@outlook.com>
2024-11-08 19:34:56 +0530

    sample response data

---
+++ response.js
@@ -0,0 +1,26 @@
+export default reponseData = [
+  {
+    key: "ISSUE-001",
+    summary: "Sample issue",
+    status: "Under Dev",
+    assignee: "Murlidhar Bhardwaj",
+  },
+  {
+    key: "ISSUE-002",
+    summary: "Sample issue",
+    status: "Under Dev",
+    assignee: "Unassigned",
+  },
+  {
+    key: "ISSUE-003",
+    summary: "Sample issue",
+    status: "Under Dev",
+    assignee: "Chetan Mistry",
+  },
+  {
+    key: "ISSUE-004",
+    summary: "Sample issue",
+    status: "Waiting for customer",
+    assignee: "Murlidhar Bhardwaj",
+  }
+];
